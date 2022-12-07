65a2a80080f2c96ef8758c410053488713d02283
Thomas <thomasmckenna12gmail.com>
2022-12-06 20:02:25 -0600

    added js delete button

--- src/main.js
+++ src/main.js
@@ -1,20 +1,33 @@
 
 
-const newItem = document.querySelector("#new-todo-input")
+const newToDoItem = document.querySelector("#new-todo-input")
 const submitBttn = document.querySelector("#new-todo-submit")
 const listItems = document.querySelector(".tasks")
 
 
+submitBttn.addEventListener('click', addToDo)
 
-function addToDo() {
+function addToDo(event) {
+    event.preventDefault()
+    console.log(newToDoItem.value)
+    const newTask = newToDoItem.value
 
-    
+    const newItem = document.createElement("p")
+    newItem.id = "new-item"
+    newItem.innerHTML = newTask
 
+    const deleteBttn = document.createElement("button")
+    deleteBttn.id = "delete-bttn"
+    deleteBttn.innerHTML = 'Delete'
+    deleteBttn.addEventListener("click", function deleteToDo(e) {
+        e.preventDefault()
+        itemDiv.remove()
+})
 
+    const itemDiv = document.createElement("div")
+    itemDiv.className = "new-tasks"
 
-
+    listItems.appendChild(itemDiv)
+    itemDiv.append(newItem, deleteBttn)
 }
 
-function deleteToDo() {
-
-}
