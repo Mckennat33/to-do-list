08b07653199ee393da54bc64f9d7ea41ad9784ef
Thomas <thomasmckenna12gmail.com>
2022-12-06 19:31:45 -0600

    added js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
 
 
+const newItem = document.querySelector("#new-todo-input")
+const submitBttn = document.querySelector("#new-todo-submit")
+const listItems = document.querySelector(".tasks")
+
+
+
+function addToDo() {
+
+    
+
+
+
+
+}
+
+function deleteToDo() {
+
+}
