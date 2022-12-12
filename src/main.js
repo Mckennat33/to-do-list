0bddb9a5eefce47233d871169ad9e2881b0904ff
Thomas <thomasmckenna12gmail.com>
2022-12-11 19:13:52 -0600

    updated new to do field

--- src/main.js
+++ src/main.js
@@ -9,25 +9,49 @@ submitBttn.addEventListener('click', addToDo)
 
 function addToDo(event) {
     event.preventDefault()
-    console.log(newToDoItem.value)
-    const newTask = newToDoItem.value
+    let newTask = newToDoItem.value
 
-    const newItem = document.createElement("p")
+// creating a tag for new task
+    let newItem = document.createElement("input")
     newItem.id = "new-item"
     newItem.innerHTML = newTask
 
+//edit new task
+    const editBttn = document.createElement("button")
+    editBttn.id = "edit-button"
+    editBttn.innerHTML = "Edit"
+    editBttn.addEventListener("click", function editButton() {
+
+    })
+
+//delete new task
     const deleteBttn = document.createElement("button")
     deleteBttn.id = "delete-bttn"
     deleteBttn.innerHTML = 'Delete'
     deleteBttn.addEventListener("click", function deleteToDo(e) {
         e.preventDefault()
-        itemDiv.remove()
-})
+        listItems.remove()
+    })
 
-    const itemDiv = document.createElement("div")
+// appending new task to div 
+// need to make an adjustment to this. 
+    const itemDiv = document.createElement("input")
     itemDiv.className = "new-tasks"
+    itemDiv.type = "text"
+    itemDiv.placeholder = newTask
 
-    listItems.appendChild(itemDiv)
-    itemDiv.append(newItem, deleteBttn)
+    //listItems.append(itemDiv, editBttn, deleteBttn)
+
+    if (!newTask == "") {
+        listItems.append(itemDiv, editBttn, deleteBttn)
+    } else {
+        alert("Please enter a task")
+    }
+
+    
+
 }
 
+function editButton() {
+    console.log("edit")
+}
